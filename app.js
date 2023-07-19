6d8c3c5537fb8afc18381949ce463385c2bd36c2
Pyoyoyo <sodbileg.d13@gmail.com>
2023-07-19 16:23:24 +0800

    finance module hiij ehelsen Expense, Income objectuudig uusgesen.

--- app.js
+++ app.js
@@ -2,21 +2,68 @@
 
 var uiController = (function() {
 
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        addBtn: ".add__btn"
+    }
+
+    var x = 1234;
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    }
 
 })();
 
 // Sanhuutei ajillah controller
 var financeController = (function() {
 
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            inc: [],
+            exp: []
+        },
+
+        totals: {
+            inc: 1000,
+            exp: 500
+        }
+    };
+
+    
 })();
 
 
 // Holboj ajillah controller
 var appController = (function(uiController, fnController) {
-
+    
     var ctrlAddItem = function() {
         // oruulah ugudliig delgetsnees olj avna.
 
+        console.log(uiController.getInput());
         // olj avsan ugugdluu sanhuugiin controllert damjuulj hadgalna.
 
         // olj avsan ugugdluudiig web deeree tohiroh hesegt gargana
@@ -24,17 +71,30 @@ var appController = (function(uiController, fnController) {
         // tusuviig tootsoolno
 
         // Etssiin uldegdel tootsoog delgetsend gargana
-        console.log("qwertrytu");
+
     }
 
-    document.querySelector(".add__btn").addEventListener('click', function() {
-        ctrlAddItem();        
-    })
+    var setupEventListeners = function() {
+        var DOM = uiController.getDOMstrings();
+        
+        document.querySelector(DOM.addBtn).addEventListener('click', function() {
+            ctrlAddItem();        
+        })
+        
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13  || event.which === 13) {
+                ctrlAddItem();
+            }
+        })
+    }
 
-    document.addEventListener('keypress', function (event) {
-        if (event.keyCode === 13  || event.which === 13) {
-            ctrlAddItem();
+    return {
+        init: function() {
+            console.log("App started...");
+            setupEventListeners();
         }
-    })
+    }
 
 })(uiController, financeController);
+
+appController.init();
